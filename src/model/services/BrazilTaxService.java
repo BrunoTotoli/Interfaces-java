bc8fa1c341a7d18369edd186055937081a6a47c2
Bruno Totoli <santosbruno101062@gmail.com>
2022-03-17 20:40:41 -0300

    using interfaces for better code

--- src/model/services/BrazilTaxService.java
+++ src/model/services/BrazilTaxService.java
@@ -1,11 +1,13 @@
 package model.services;
 
-public class BrazilTaxService {
+public class BrazilTaxService implements TaxService {
 
-    public Double tax(Double amount) {
+    @Override
+    public double tax(double amount) {
         if (amount <= 100.00)
             return amount * 0.2;
         return amount + 0.15;
     }
+
 
 }
